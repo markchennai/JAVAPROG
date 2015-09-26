61ce40cc3207973ce847797e945548fbf1c9f4bf
markchennai <mahesh.rajakumar@gmail.com>
2015-09-26 07:27:00 +0530

    indented source

--- javasource/bst.java
+++ javasource/bst.java
@@ -2,25 +2,22 @@ package com.prep;
 
 import java.util.Scanner;
 
-class BSTNode
-{
+class BSTNode {
 	BSTNode left, right;
 	int data;
-	
-	public BSTNode()
-	{
+
+	public BSTNode() {
 		left = null;
 		right = null;
 		data = 0;
 	}
-	
-	public BSTNode(int n)
-	{
+
+	public BSTNode(int n) {
 		left = null;
 		right = null;
 		data = n;
 	}
-	
+
 	public BSTNode getLeft() {
 		return left;
 	}
@@ -44,168 +41,137 @@ class BSTNode
 	public void setData(int data) {
 		this.data = data;
 	}
-	
+
 }
 
-class BST
-{
+class BST {
 	private BSTNode root;
-	
-	public BST()
-	{
+
+	public BST() {
 		root = null;
 	}
-	
-	public boolean isEmpty()
-	{
+
+	public boolean isEmpty() {
 		return root == null;
 	}
-	
-	public void insert(int data)
-	{
+
+	public void insert(int data) {
 		root = insert(root, data);
 	}
-	
-	private BSTNode insert(BSTNode node, int data)
-	{
-		if (node == null)
-		{
+
+	private BSTNode insert(BSTNode node, int data) {
+		if (node == null) {
 			node = new BSTNode(data);
-		}
-		else
-		{
-			if(data <= node.getData())
+		} else {
+			if (data <= node.getData())
 				node.left = insert(node.left, data);
 			else
 				node.right = insert(node.right, data);
 		}
-		
+
 		return node;
 	}
-	
-	public boolean search(int val)
-	{
+
+	public boolean search(int val) {
 		return search(root, val);
 	}
-	
-	private boolean search(BSTNode r, int val)
-	{
+
+	private boolean search(BSTNode r, int val) {
 		boolean found = false;
-		
-		while((r!= null) && !found)
-		{
+
+		while ((r != null) && !found) {
 			int rval = r.getData();
 			if (val < rval)
 				r = r.getLeft();
-			else if(val > rval)
+			else if (val > rval)
 				r = r.getRight();
-			else
-			{
+			else {
 				found = true;
 				break;
 			}
 		}
-		
+
 		return found;
-	} 
-	
-	private BSTNode delete(BSTNode root, int k)
-	{
+	}
+
+	private BSTNode delete(BSTNode root, int k) {
 		BSTNode p, p2, n;
-		
-		if (root.getData() == k)
-		{
+
+		if (root.getData() == k) {
 			BSTNode lt, rt;
 			lt = root.getLeft();
 			rt = root.getRight();
-			
-			if(lt == null && rt == null)
+
+			if (lt == null && rt == null)
 				return null;
-			else if(lt == null)
-			{
+			else if (lt == null) {
 				p = rt;
 				return p;
-			}
-			else if(rt == null)
-			{
+			} else if (rt == null) {
 				p = lt;
 				return p;
-			}
-			else
-			{
+			} else {
 				p2 = rt;
 				p = rt;
-				while(p.getLeft() != null)
+				while (p.getLeft() != null)
 					p = p.getLeft();
 				p.setLeft(lt);
 				return p2;
 			}
 		}
-			
-		if(k < root.getData())
-		{
+
+		if (k < root.getData()) {
 			n = delete(root.getLeft(), k);
-			root.setLeft(n);				
-		}
-		else
-		{
+			root.setLeft(n);
+		} else {
 			n = delete(root.getRight(), k);
 			root.setRight(n);
 		}
-		
+
 		return root;
 	}
-	
-	public void delete(int k)
-	{
-		if(isEmpty())
+
+	public void delete(int k) {
+		if (isEmpty())
 			System.out.println("Tree Empty");
-		else if(search(k) == false)
+		else if (search(k) == false)
 			System.out.println("Sorry " + k + " is not present");
-		else
-		{ 
+		else {
 			root = delete(root, k);
 			System.out.println(k + "deleted from the tree");
 		}
 	}
-	
+
 	/* Function for in-order traversal */
-    public void inorder()
-    {
-        inorder(root);
-    }
-    
-    public int size()
-    {
-    	Integer val = 0;
-    	
-    	size_order(root, val);
-    	
-    	return val.intValue();
-    	
-    }
-    
-    private void size_order(BSTNode r, Integer v)
-    {
-    	if (r != null)
-        {
-            inorder(r.getLeft());
-            v += 1;
-            inorder(r.getRight());
-        }
-    }
-    
-    private void inorder(BSTNode r)
-    {
-        if (r != null)
-        {
-            inorder(r.getLeft());
-            System.out.print(r.getData() +" ");
-            inorder(r.getRight());
-        }
-    }
-	
-	
+	public void inorder() {
+		inorder(root);
+	}
+
+	public int size() {
+		Integer val = 0;
+
+		size_order(root, val);
+
+		return val.intValue();
+
+	}
+
+	private void size_order(BSTNode r, Integer v) {
+		if (r != null) {
+			inorder(r.getLeft());
+			v += 1;
+			inorder(r.getRight());
+		}
+	}
+
+	private void inorder(BSTNode r) {
+		if (r != null) {
+			inorder(r.getLeft());
+			System.out.print(r.getData() + " ");
+			inorder(r.getRight());
+		}
+	}
+
 }
 
 public class BinarySearchTree {
@@ -216,45 +182,42 @@ public class BinarySearchTree {
 		BST bst = new BST();
 		System.out.println("Binary Search Tree Test \n");
 		char ch;
-		
-		do
-		{
+
+		do {
 			System.out.println("Binary Search Tree Operations \n");
 			System.out.println("1. Insert ");
 			System.out.println("2. Delete ");
 			System.out.println("3. Size ");
-					
+
 			int choice = scan.nextInt();
-			switch(choice)
-			{
+			switch (choice) {
 			case 1:
 				System.out.println("Enter integer element to insert :");
 				bst.insert(scan.nextInt());
 				break;
-				
+
 			case 2:
 				System.out.println("Enter integer element to delete :");
 				bst.delete(scan.nextInt());
 				break;
-				
+
 			case 3:
 				System.out.println("Fetching size of BST  : " + bst.size());
 				break;
-				
-			default :
+
+			default:
 				System.out.println("Wrong Entry : ");
 				break;
-					
+
 			}
-			
-			//Display Tree
+
+			// Display Tree
 			System.out.println("In-order ");
 			bst.inorder();
 			System.out.println("Do you want to continue (Type y or n) \n");
 			ch = scan.next().charAt(0);
-		}while(ch =='Y' || ch =='y');
-		
+		} while (ch == 'Y' || ch == 'y');
+
 	}
 
 }
- 
